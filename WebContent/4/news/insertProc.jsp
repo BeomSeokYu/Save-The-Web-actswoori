56fd55a75001f32e3c2e084785534660fc674583
dawnpoem <dawnpoem@naver.com>
2023-02-16 17:12:46 +0900

    news Paging
    뉴스 페이징 처리 완료

--- WebContent/4/news/insertProc.jsp
+++ WebContent/4/news/insertProc.jsp
@@ -8,11 +8,16 @@ String ntitle = request.getParameter("title");
 String ncontent = request.getParameter("content");
 String nemail = request.getParameter("email");
 
+
 boolean result = NewsDAO.insertNews(ntitle, ncontent, nemail);
+
+/* for(int i=0; i<30; i++) {
+	result = NewsDAO.insertNews(ntitle+i, ncontent+i , nemail);
+} */
 
 if (result) {
 	response.sendRedirect("newsAll.jsp?msg=insertSuccess");
 } else {
 response.sendRedirect("insertForm.jsp?msg=fail");
-}
+} 
 %>
--- src/jdbc/NewsDAO.java
+++ src/jdbc/NewsDAO.java
@@ -125,6 +125,80 @@ public class NewsDAO {
 		}
 	}
 	
+	
+	public static List<NewsDTO> selectAllNewsPaging(int pageNum, int amount) throws NamingException, SQLException {
+		//뉴스 목록 조회(페이징)
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+		String sql = "SELECT * FROM news ORDER BY nno DESC limit ?, ?";
+		
+		conn = ConnectionPool.get();
+		pstmt = conn.prepareStatement(sql);
+		
+		pstmt.setInt(1, (pageNum - 1) * amount);
+		pstmt.setInt(2, amount);
+		
+		rs = pstmt.executeQuery();
+		
+		List<NewsDTO> newsList = new ArrayList<NewsDTO>();
+		
+		while (rs.next()) {
+			NewsDTO ndto = new NewsDTO();
+			
+			ndto.setNno(rs.getString(1));
+			ndto.setNtitle(rs.getString(2));
+			ndto.setNcontent(rs.getString(3));
+			ndto.setEmail(rs.getString(4));
+			ndto.setNdate(rs.getString(5));
+			
+			newsList.add(ndto);
+		}
+			
+		return newsList;
+		
+		} finally {
+			if(rs != null) rs.close();
+			if(pstmt != null) pstmt.close();
+			if(conn != null) conn.close();
+		}
+	}
+	
+	public int getTotal() throws SQLException {
+
+		int result = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+		String sql = "select count(*) as total from news";
+		
+		conn = ConnectionPool.get();
+		pstmt = conn.prepareStatement(sql);
+		
+		rs = pstmt.executeQuery();
+		
+		while (rs.next()) {
+			result = rs.getInt("total");
+			
+		}
+		
+		} catch (SQLException | NamingException e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null) rs.close();
+			if(pstmt != null) pstmt.close();
+			if(conn != null) conn.close();
+		}
+		
+		
+		return result;
+	}
+	
 	public static boolean updateNews(int nno, String ntitle, String ncontent) throws SQLException, NamingException {
 		
 		Connection conn = null;
