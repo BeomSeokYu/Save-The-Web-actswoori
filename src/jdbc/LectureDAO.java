1588e5adac95af7e4777f319a1b321fe1d7f213f
BeomSeok Yu <69231700+BeomSeokYu@users.noreply.github.com>
2023-02-24 09:21:10 +0900

    lecture DAO 수정

--- src/jdbc/LectureDAO.java
+++ src/jdbc/LectureDAO.java
@@ -19,15 +19,19 @@ import java.util.ArrayList;
 
 import javax.naming.NamingException;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import util.ConnectionPool;
 
 public class LectureDAO {
 	
 	//특강 등록
-	public static boolean insert(String ltitle, String lname, String lcontent, String email)throws NamingException, SQLException{
+	public static boolean insert(String ltitle, String lname, String lcontent, String email) {
 		
 		Connection conn = null;
 		PreparedStatement pstmt = null;
+		boolean result = false;
 		
 		try {
 			String sql = "INSERT INTO lecture (ltitle,lname,lcontent,email) VALUES(?,?,?,?)";
@@ -39,26 +43,27 @@ public class LectureDAO {
 			pstmt.setString(3, lcontent);
 			pstmt.setString(4, email);
 			
-		return (pstmt.executeUpdate() == 1) ? true :false;	
+			result =  (pstmt.executeUpdate() == 1) ? true :false;	
 		
-		}finally {
-			if(pstmt != null) pstmt.close();
-			if(conn != null) conn.close();
+		} catch (Exception e) {
+		} finally {
+			close(conn, pstmt, null);
 		}
+		return result;
 	}
 	
 	//특강 목록 전체
-	public static ArrayList<LectureDTO> getallList() throws SQLException,NamingException{
+	public static ArrayList<LectureDTO> getallList() {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		
+		ArrayList<LectureDTO> boards = new ArrayList<LectureDTO>();
 		try {
 			String sql = "SELECT * FROM lecture";
 			conn = ConnectionPool.get();
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
-			ArrayList<LectureDTO> boards = new ArrayList<LectureDTO>();
 			while(rs.next()) {
 				boards.add(new LectureDTO(rs.getInt(1),
 										  rs.getString(2),
@@ -67,27 +72,28 @@ public class LectureDAO {
 										  rs.getString(5),
 										  rs.getString(6)));
 			}
-			return boards;
+		} catch (Exception e) {
+			boards = null;
+		} finally {
+			close(conn, pstmt, rs);
 		}
-		
-		finally {
-			if(rs != null) rs.close();
-			if(pstmt != null) pstmt.close();
-			if(conn != null) conn.close();
-		}
+		return boards;
 
 	}	
 	
 	//특강 디테일
-	public static LectureDTO getDetail(int lno) throws NamingException, SQLException{
+	public static LectureDTO getDetail(int lno) {
 		
 		String sql = "SELECT * FROM lecture where lno=?";
-		
-		Connection conn = ConnectionPool.get();
-		
-		PreparedStatement pstmt = conn.prepareStatement(sql);
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		LectureDTO lecture = null;
+		try {
+			conn = ConnectionPool.get();
+			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1,lno);
-			ResultSet rs = pstmt.executeQuery();
+			rs = pstmt.executeQuery();
 			
 			rs.next();
 			
@@ -97,20 +103,24 @@ public class LectureDAO {
 			String email = rs.getString(5);
 			String ldate = rs.getString(6);
 		
-			LectureDTO lecture = new LectureDTO(lno,ltitle,lname,lcontent,email,ldate); //새로운 객체 생성
+			lecture = new LectureDTO(lno,ltitle,lname,lcontent,email,ldate); //새로운 객체 생성
 			
 			rs.close();
 			pstmt.close();
 			conn.close();
-		
-			return lecture;
+		} catch (Exception e) {
+		} finally {
+			close(conn, pstmt, rs);
+		}
+		return lecture;
 	  }
 	
 	//게시글 수정
-	public static boolean update(int lno, String ltitle, String lname, String lcontent)throws NamingException, SQLException{
+	public static boolean update(int lno, String ltitle, String lname, String lcontent) {
 		
 		Connection conn = null;
 		PreparedStatement pstmt = null;
+		boolean result = false;
 		
 		try {
 			String sql = "UPDATE lecture SET ltitle=?,lname=?,lcontent=? WHERE lno = ?";
@@ -122,19 +132,22 @@ public class LectureDAO {
 			pstmt.setString(3, lcontent);
 			pstmt.setInt(4, lno);
 			
-		return (pstmt.executeUpdate() == 1) ? true :false;	
+			result = (pstmt.executeUpdate() == 1) ? true :false;	
 		
-		}finally {
-			if(pstmt != null) pstmt.close();
-			if(conn != null) conn.close();
+		} catch (Exception e) {
+		} finally {
+			close(conn, pstmt, null);
 		}
+		
+		return result;
 	}
 	
 	//게시글 삭제
-		public static boolean delete(int lno)throws NamingException, SQLException{
+	public static boolean delete(int lno) {
 		
 		Connection conn = null;
 		PreparedStatement pstmt = null;
+		boolean result = false;
 		
 		try {
 			String sql = "DELETE FROM lecture WHERE lno = ?";
@@ -143,110 +156,119 @@ public class LectureDAO {
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, lno);
 			
-		return (pstmt.executeUpdate() == 1) ? true :false;	
+			result =  (pstmt.executeUpdate() == 1) ? true :false;	
 		
-		}finally {
-			if(pstmt != null) pstmt.close();
-			if(conn != null) conn.close();
+		} catch (Exception e) {
+		} finally {
+			close(conn, pstmt, null);
 		}
+		return result;
 	}
 		
-		//작성자 이메일 조회
-		public static String selectEmail(int lno) {
-			try {
-				String sql = "SELECT email FROM lecture WHERE lno = ?";
-				
-				Connection conn = ConnectionPool.get();
-				PreparedStatement pstmt = conn.prepareStatement(sql);
-				pstmt.setInt(1, lno);
-				
-				ResultSet rs = pstmt.executeQuery();
-				
-				String email = null;
-				
-				if (rs.next()) {
-					email = rs.getString(1);
-				}
-				
-				rs.close();
-				pstmt.close();
-				conn.close();
-				
-				return email;
-			} catch (NamingException | SQLException e) {
-				e.printStackTrace();
-				return null;
-			}
-		}
-		
-		
-		//페이징
-		public static ArrayList<LectureDTO> getListpaging(int pageNum, int amount) throws NamingException, SQLException {
+	//작성자 이메일 조회
+	public static String selectEmail(int lno) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String email = null;
+		try {
+			String sql = "SELECT email FROM lecture WHERE lno = ?";
 			
-			Connection conn = null;
-			PreparedStatement pstmt = null;
-			ResultSet rs = null;
+			conn = ConnectionPool.get();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, lno);
 			
-			try {
-				String sql = "SELECT * FROM lecture ORDER BY lno DESC limit ?,?";
-				
-				conn = ConnectionPool.get();
-				pstmt = conn.prepareStatement(sql);
-						pstmt.setInt(1,(pageNum-1)*amount);
-						pstmt.setInt(2,amount);
-						
-				rs = pstmt.executeQuery(); 
-				
-				ArrayList<LectureDTO> lectures = new ArrayList<LectureDTO>();
-				
-				while(rs.next()) {
-					lectures.add(new LectureDTO(rs.getInt(1),
-										  rs.getString(2),
-										  rs.getString(3),
-										  rs.getString(4),
-										  rs.getString(5),
-										  rs.getString(6)));
-
-				}return lectures;
-				
-			}finally {
-				if(rs != null) rs.close();
-				if(pstmt != null) pstmt.close();
-				if(conn != null) conn.close();
+			rs = pstmt.executeQuery();
+			
+			
+			if (rs.next()) {
+				email = rs.getString(1);
 			}
+		} catch (Exception e) {
+		} finally {
+			close(conn, pstmt, rs);
 		}
+		return email;
+	}
+	
+	
+	//페이징
+	public static ArrayList<LectureDTO> getListpaging(int pageNum, int amount) {
 		
-		//페이지 (총 개수)
-		public int getTotal() throws SQLException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		ArrayList<LectureDTO> lectures = new ArrayList<LectureDTO>();
+		try {
+			String sql = "SELECT * FROM lecture ORDER BY lno DESC limit ?,?";
+			
+			conn = ConnectionPool.get();
+			pstmt = conn.prepareStatement(sql);
+					pstmt.setInt(1,(pageNum-1)*amount);
+					pstmt.setInt(2,amount);
+					
+			rs = pstmt.executeQuery(); 
+			
+			
+			while(rs.next()) {
+				lectures.add(new LectureDTO(rs.getInt(1),
+									  rs.getString(2),
+									  rs.getString(3),
+									  rs.getString(4),
+									  rs.getString(5),
+									  rs.getString(6)));
 
-		    int result = 0;
-		    Connection conn = null;
-		    PreparedStatement pstmt = null;
-		    ResultSet rs = null;
-		    
-		    try {
-		    String sql = "select count(*) as total from lecture";
-		    
-		    conn = ConnectionPool.get();
-		    pstmt = conn.prepareStatement(sql);
-		    
-		    rs = pstmt.executeQuery();
-		    
-		    while (rs.next()) {
-		       result = rs.getInt("total");
-		       
-		    }
-		    
-		    } catch (SQLException | NamingException e) {
-		       e.printStackTrace();
-		    } finally {
-		       if(rs != null) rs.close();
-		       if(pstmt != null) pstmt.close();
-		       if(conn != null) conn.close();
-		    }
-		    
-		    
-		    return result;
-		 }
+			}
+			
+		} catch (Exception e) {
+			lectures = null;
+		} finally {
+			close(conn, pstmt, rs);
+		}
+		return lectures;
+	}
+		
+	//페이지 (총 개수)
+	public int getTotal() {
+
+	    int result = 0;
+	    Connection conn = null;
+	    PreparedStatement pstmt = null;
+	    ResultSet rs = null;
+	    
+	    try {
+	    String sql = "select count(*) as total from lecture";
+	    
+	    conn = ConnectionPool.get();
+	    pstmt = conn.prepareStatement(sql);
+	    
+	    rs = pstmt.executeQuery();
+	    
+	    while (rs.next()) {
+	       result = rs.getInt("total");
+	       
+	    }
+	    
+	    } catch (SQLException | NamingException e) {
+	       e.printStackTrace();
+	    } finally {
+	    	close(conn, pstmt, rs);
+	    }
+	    
+	    
+	    return result;
+	 }
+	
+	// 객체 닫기
+	public static void close(Connection conn, PreparedStatement pstmt, ResultSet rSet) {
+		try {
+			if(rSet!=null) rSet.close();
+			if(pstmt!=null) pstmt.close();
+			if(conn!=null) conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
 
